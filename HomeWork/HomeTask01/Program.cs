f1b1059a2093655d109de27cbec2ea5697eaa13a
Egoist <gor-e@mail.ru>
2022-12-11 22:19:05 +0300

    решена первая задача

---
+++ HomeWork/HomeTask01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача №1:Нахождение максимального из трёх вводных чисел.
+
+Console.WriteLine("Start");
+
+int a = 2; // вариант №2 а = 44, вариант №3 а = 22
+int b = 3; // вариант №2 b = 5, вариант №3 b = 3
+int c = 7; // вариант №2 c = 78, вариант №3 c = 9
+
+int max = a;
+
+if (b > a) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.WriteLine("End");
