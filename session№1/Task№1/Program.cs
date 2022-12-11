8f90730fdf1adf993959fb0d893187a2d98028f3
Egoist <gor-e@mail.ru>
2022-12-11 17:17:17 +0300

    decision first exercise

---
+++ session№1/Task№1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Print the square of the number
+Console.WriteLine("Start");
+int number = 17; // any integer
+int result = number * number; // calculating the square
+Console.WriteLine(result);
+Console.WriteLine("End");
+  
